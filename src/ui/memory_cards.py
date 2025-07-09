9831e0d59b7c837c18e3bec95c8f1cd01d33dd51
Allam Elsheikh <148693823+AllamElsheikh@users.noreply.github.com>
2025-07-09 17:56:22 +0300

    Create memory_cards.py

---
+++ src/ui/memory_cards.py
@@ -0,0 +1,33 @@
+class MemoryCardUI:
+    """Memory card user interface"""
+    
+    def __init__(self):
+        pass
+    
+    def create_memory_card(self, person_data):
+        """Create visual memory card"""
+        pass
+    
+    def display_memory_cards(self, cards_list):
+        """Display list of memory cards"""
+        pass
+    
+    def edit_memory_card(self, card_id):
+        """Edit existing memory card"""
+        pass
+    
+    def delete_memory_card(self, card_id):
+        """Delete memory card"""
+        pass
+    
+    def search_cards(self, query):
+        """Search through memory cards"""
+        pass
+    
+    def add_photo_to_card(self, card_id, photo_path):
+        """Add photo to memory card"""
+        pass
+    
+    def create_new_card_dialog(self):
+        """Create new card dialog"""
+        pass
