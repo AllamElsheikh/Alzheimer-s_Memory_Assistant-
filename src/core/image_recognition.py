83c1ecc11deb82905ee24c30dabc5f7c0b0898f7
Allam Elsheikh <148693823+AllamElsheikh@users.noreply.github.com>
2025-07-09 17:55:23 +0300

    Create image_recognition.py

---
+++ src/core/image_recognition.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+
+class ImageRecognition:
+    """Image and face recognition system"""
+    
+    def __init__(self):
+        pass
+    
+    def load_face_model(self):
+        """Load face recognition model"""
+        pass
+    
+    def detect_faces(self, image_path):
+        """Detect faces in image"""
+        pass
+    
+    def recognize_face(self, face_encoding):
+        """Recognize face from encoding"""
+        pass
+    
+    def add_face_to_database(self, face_encoding, person_id):
+        """Add face to recognition database"""
+        pass
+    
+    def train_face_model(self, training_data):
+        """Train face recognition model"""
+        pass
+    
+    def process_image(self, image_path):
+        """Process image for recognition"""
+        pass
+    
+    def get_face_encoding(self, image):
+        """Get face encoding from image"""
+        pass
