0d4dcebcb112bf05b79fa282edcd6ae62fe1d23a
Allam Elsheikh <148693823+AllamElsheikh@users.noreply.github.com>
2025-07-09 17:51:52 +0300

    Create settings.py

---
+++ src/config/settings.py
@@ -0,0 +1,25 @@
+class Settings:
+    """Application configuration settings"""
+    
+    def __init__(self):
+        pass
+    
+    def load_config(self):
+        """Load configuration from file"""
+        pass
+    
+    def save_config(self):
+        """Save configuration to file"""
+        pass
+    
+    def get_ai_model_path(self):
+        """Get path to AI model files"""
+        pass
+    
+    def get_data_directory(self):
+        """Get data storage directory"""
+        pass
+    
+    def get_accessibility_settings(self):
+        """Get accessibility configuration"""
+        pass
